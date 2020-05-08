d4399312aa913f435992edc319b50912403c8d3d
alexandre <alexandrevoliveira30@gmail.com>
2020-05-07 22:56:28 -0300

    modified teachers.js

--- teachers.js
+++ teachers.js
@@ -62,7 +62,6 @@ exports.show = function(req, res) {
 }
 
 // edit
-
 exports.edit = function(req, res) {
     const { id } = req.params
 
@@ -80,4 +79,50 @@ exports.edit = function(req, res) {
     }
 
     return res.render("teachers/edit", { teacher })
+}
+
+// put
+exports.put = function(req, res) {
+    const { id } = req.body
+    let index = 0
+
+    const foundTeacher = data.teachers.find(function(teacher, foundIndex){
+        if ( teacher.id == id ) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundTeacher) return res.send("Teacher not found")
+
+    const teacher = {
+        ...foundTeacher,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.teachers[index] = teacher
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if (err) return res.send("Error in writing data")
+
+        return res.redirect(`/teachers/${id}`)
+    })
+}
+
+// delete
+exports.delete = function (req, res) {
+    const { id } = req.body
+
+    const filteredTeachers = data.teachers.filter(function(teacher) {
+        return teacher.id != id
+    })
+
+    data.teachers = filteredTeachers
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if (err) return res.send("Error in writing data")
+
+        return res.redirect("/teachers")
+    })
 }
