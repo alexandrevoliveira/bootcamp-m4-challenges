502a00b0676dc46f9d8eb4c6735ee1b6b900baa2
alexandre <alexandrevoliveira30@gmail.com>
2020-05-07 21:24:38 -0300

    minor modifications

--- routes.js
+++ routes.js
@@ -16,9 +16,9 @@ routes.get("/teachers/create", function(req, res) {
 
 routes.get("/teachers/:id", teachers.show)
 
-routes.post("/teachers", teachers.post)
-
 routes.get("/teachers/:id/edit", teachers.edit)
+
+routes.post("/teachers", teachers.post)
 
 routes.get("/students", function(req, res) {
     return res.render("students/students")
