4ae605ca84708ab7a2ad24a87c0b407daa7820c0
alexandre <alexandrevoliveira30@gmail.com>
2020-05-08 23:34:53 -0300

    modified teachers.js

--- teachers.js
+++ teachers.js
@@ -2,6 +2,11 @@ const fs = require('fs')
 const data = require('./data.json')
 const { age, date } = require('./utils')
 
+// index
+exports.index = function(req, res) {
+    return res.render("teachers/index", { teachers: data.teachers})
+}
+
 // create
 exports.post = function(req, res) {
 
@@ -98,7 +103,8 @@ exports.put = function(req, res) {
     const teacher = {
         ...foundTeacher,
         ...req.body,
-        birth: Date.parse(req.body.birth)
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
     }
 
     data.teachers[index] = teacher
